a390430c9c39fc51a263b0bec3f8a04908cbf71e
Soogeun Park <bwmelon97@unist.ac.kr>
2019-05-29 23:34:11 +0900

    회원가입 / 로그인 일부 구현
    accounts app의 views, urls, template 생성하여 회원가입했을 때, 데이터베이스의 user부분에 저장되게 만듦

--- BasicTodo/accounts/views.py
+++ BasicTodo/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password-confirm']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password']
+            )
+            auth.login(request, user)
+            return render(request, 'accounts/signup.html')
+        return render(request, 'accounts/signup.html')
+
+    return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        input_id = request.POST['username']
+        input_pw = request.POST['password']
+        user = auth.authenticate(request, username=input_id, password=input_pw)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('accounts:test')
+        return render(request, 'accounts/login.html', {'error': "username or password is not correct."})
+
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('accounts:test')
+    return redirect('accounts:test')
+
+
+def test(request):
+    pass
