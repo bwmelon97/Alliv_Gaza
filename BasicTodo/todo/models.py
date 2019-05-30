61cc9d521fe3802f2337c98de9f0857604f1b721
Soogeun Park <bwmelon97@unist.ac.kr>
2019-05-30 17:01:31 +0900

    todolist 모델 생성

--- BasicTodo/todo/models.py
+++ BasicTodo/todo/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class TodoList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo_content = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    pub_date = models.DateTimeField('date published')
+    last_mod = models.DateTimeField('lastest modified date time')
+
+    def __str__(self):
+        return self.todo_content
+
+    def modify_content(self, newStr):
+        self.todo_content = newStr
+        self.last_mod = timezone.now()
+
+    def change_complete(self):
+        if self.is_completed:
+            self.is_completed = False
+        else:
+            self.is_completed = True
+        self.last_mod = timezone.now()
+
